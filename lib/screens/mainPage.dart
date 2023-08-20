6a2b1721c7db09a1381b6f3dad69e8b07661faea
nala0897@naver.com <nala0897@naver.com>
2023-08-20 22:05:07 +1200

    Add:app mainPage UI

--- lib/screens/mainPage.dart
+++ lib/screens/mainPage.dart
@@ -6,29 +6,87 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'Hello',
-            style: TextStyle(
-                fontSize: 16
-            ),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
 
-          SizedBox(height: 20,),
+            // 'Hello'
+            Container(
+              height: 210.0,
+              padding: const EdgeInsets.only(top: 100, left: 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    'Hello',
+                    style: TextStyle(
+                        fontSize: 35
+                    ),
+                  ),
 
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black.withOpacity(0.3)
-                  )
-                ]
+                  SizedBox(width: 5,),
+
+                  Icon(
+                    Icons.coffee,
+                    size: 40,
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+
+            // 주문 상태
+            Container(
+              height: 200,
+              // margin: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      blurRadius: 5,
+                      spreadRadius: 2
+                    )
+                  ]
+              ),
+            ),
+
+            SizedBox(height: 30,),
+
+            // 소식
+            Row(
+              children: [
+                Icon(
+                  Icons.mail_outline_sharp,
+                  size: 20,
+                ),
+
+                SizedBox(width: 5,),
+
+                Text(
+                  "What's New",
+                  style: TextStyle(
+                    fontSize: 15
+                  ),
+                )
+              ],
+            ),
+
+            // 디저트
+            Row(
+              children: [
+                Text(
+                  '하루가 달콤해지는 시간',
+                  style: TextStyle(
+                    fontSize: 25
+                  ),
+                ),
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
