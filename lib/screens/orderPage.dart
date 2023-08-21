92e4053ef3674ad3ee2df3740085ea7eade8391a
nala0897@naver.com <nala0897@naver.com>
2023-08-21 21:53:03 +1200

    Add:OrderPage UI 추가 중

--- lib/screens/orderPage.dart
+++ lib/screens/orderPage.dart
@@ -6,6 +6,39 @@ class OrderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 70, right: 20, left: 20, bottom: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 50,),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Order',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+        ),
+      ),
     );
   }
 }
