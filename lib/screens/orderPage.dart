43fa19824e403ebbed7a4857f95d1f1f0f86fcc5
nala0897@naver.com <nala0897@naver.com>
2023-08-23 18:09:53 +1200

    Add:tabBar UI

--- lib/screens/orderPage.dart
+++ lib/screens/orderPage.dart
@@ -9,39 +9,48 @@ class OrderPage extends StatelessWidget {
       // tabBar menu 수 지정
       length: 3,
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 70, right: 20, left: 20, bottom: 20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Icon(
-                      Icons.search,
-                      size: 30,
-                    ),
-                  ],
-                ),
+        appBar: AppBar(
+          // 뒤로가기 화살표 없애기
+          automaticallyImplyLeading: false,
 
-                SizedBox(height: 50,),
+          title: Padding(
+            padding: const EdgeInsets.only(top:10),
+            child: Text(
+              'Order',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
 
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Order',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ],
-                )
-              ],
+          actions: [
+            IconButton(
+              onPressed: (){},
+              icon: Icon(
+                Icons.search,
+                size: 30,
+              )
             )
+          ],
+
+          bottom: TabBar(
+            padding: EdgeInsets.only(top: 20),
+            tabs: [
+              Tab(text: '음료',),
+              Tab(text: '푸드',),
+              Tab(text: '상품',)
+            ],
           ),
         ),
+
+        body: const TabBarView(
+          children: [
+            Text('tab1'),
+            Text('tab2'),
+            Text('tab3')
+          ],
+        )
       ),
     );
   }
