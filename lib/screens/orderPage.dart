fe540639cc5836f42d12a628b3c1edc8082888e4
nala0897@naver.com <nala0897@naver.com>
2023-08-23 17:47:21 +1200

    Add:tabBar 메뉴 구성을 위해 Scaffold 를 DefaultTabController 로 감싸주었다.

--- lib/screens/orderPage.dart
+++ lib/screens/orderPage.dart
@@ -5,38 +5,42 @@ class OrderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 70, right: 20, left: 20, bottom: 20),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(
-                    Icons.search,
-                    size: 30,
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 50,),
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    'Order',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold
+    return DefaultTabController(
+      // tabBar menu 수 지정
+      length: 3,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 70, right: 20, left: 20, bottom: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.search,
+                      size: 30,
                     ),
-                  ),
-                ],
-              )
-            ],
-          )
+                  ],
+                ),
+
+                SizedBox(height: 50,),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Order',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ),
         ),
       ),
     );
