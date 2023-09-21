1847834a979ebcc9428e762335f45c87850be302
nala0897@naver.com <nala0897@naver.com>
2023-09-21 16:26:13 +1200

    Add:커피 옵션 UI

--- lib/screens/personalOption/selectedEspressoItem.dart
+++ lib/screens/personalOption/selectedEspressoItem.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:oasis_cafe_app/config/palette.dart';
 
@@ -9,25 +10,111 @@ class SelectedEspressoItem extends StatelessWidget {
   final documentSnapshot;
   final itemName;
 
+  Color setBackgroundColor() {
+    return const Color.fromARGB(250, 250, 250, 250);
+  }
+
+  TextStyle setTitleTextStyle() {
+    return const TextStyle(
+        fontSize: 17
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        ExpansionPanelList.radio(
+          elevation: 0,
+          children: [
+            ExpansionPanelRadio(
+              // header 를 클릭했을 때도 펼치고 접을 수 있도록 설정
+              canTapOnHeader: true,
+              backgroundColor: setBackgroundColor(),
+              value: 0,
+              headerBuilder: (BuildContext context, bool isExpanded) {
+                return ListTile(
+                  title: Text(
+                    '커피',
+                    style: setTitleTextStyle(),
+                  ),
+
+                  subtitle: Text(
+                    '에스프레소 샷 ${documentSnapshot['espresso']}',
+                  ),
+                );
+              },
+
+                body: Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey, width: 1),
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text(
+                            '디카페인'
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 30,),
+
+                      Row(
+                        children: [
+                          Expanded(
+                              child: Text(
+                              '에스프레소 샷',
+                              style: TextStyle(
+                                fontSize: 15
+                              ),
+                            )
+                          ),
+                          Expanded(child: Text('')),
+                          Expanded(
+                              child: Row(
+                              children: [
+                                Icon(CupertinoIcons.minus_circle),
+                                SizedBox(width: 20,),
+                                Text(
+                                  '${documentSnapshot['espresso']}',
+                                  style: TextStyle(
+                                      fontSize: 15
+                                  )
+                                ),
+                                SizedBox(width: 20,),
+                                Icon(CupertinoIcons.plus_circle)
+                              ],
+                            )
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+            ),
+
+          ],
+        ),
         // 커피
-          ListTile(
-            title: const Text(
-              Strings.coffee,
-              style: TextStyle(
-                  fontSize: 17
-              ),
-            ),
-
-            subtitle: Text(
-                '에스프레소 샷 ${documentSnapshot['espresso']}'
-            ),
-
-            trailing: Icon(Icons.arrow_forward_ios),
-          ),
+        //   ListTile(
+        //     title: const Text(
+        //       Strings.coffee,
+        //       style: TextStyle(
+        //           fontSize: 17
+        //       ),
+        //     ),
+        //
+        //     subtitle: Text(
+        //         '에스프레소 샷 ${documentSnapshot['espresso']}'
+        //     ),
+        //
+        //     trailing: Icon(Icons.arrow_forward_ios),
+        //   ),
 
         Divider(height: 5, thickness: 1,),
 
