9dc8aadfb662526728d32f03cd1b604c1462ff31
nala0897@naver.com <nala0897@naver.com>
2023-09-24 16:57:17 +1300

    Fix:메뉴 수 조절 및 그에 따른 UI 수정

--- lib/screens/otherPage.dart
+++ lib/screens/otherPage.dart
@@ -5,8 +5,7 @@ import 'package:provider/provider.dart';
 
 const double sizedBoxWidth = 110.0;
 const double sizedBoxHeight = 110.0;
-final List<String> cardMenuRow1 = ['전자영수증', '나만의 메뉴', '장바구니'];
-final List<String> cardMenuRow2 = ['내 카드 관리', '개인정보 관리', '설정'];
+final List<String> cardMenuRow = ['전자영수증', '개인정보 관리', '설정'];
 final List<String> customerServiceMenuList = ['고객의 소리', '매장 정보', '내 리뷰'];
 final List<IconData> customerServiceMenuIcon = [CupertinoIcons.speaker_2, CupertinoIcons.location_solid, Icons.edit_note];
 
@@ -38,28 +37,15 @@ class OtherPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-             // 카드 메뉴1
+             // 카드 메뉴
              Row(
                children: [
                  Spacer(),
-                 for( int i = 0; i < cardMenuRow1.length; i++ )
-                   CardMenuRow1(i),
+                 for( int i = 0; i < cardMenuRow.length; i++ )
+                   CardMenuRow(i),
                  Spacer(),
                ],
              ),
-
-            SizedBox(height: 10,),
-
-            // 카드 메뉴2
-            Row(
-              children: [
-                Spacer(),
-                // 카드 메뉴2
-                for( int i = 0; i < cardMenuRow2.length; i++ )
-                  CardMenuRow2(i),
-                Spacer(),
-              ],
-            ),
 
             SizedBox(height: 35,),
 
@@ -92,8 +78,8 @@ class OtherPage extends StatelessWidget {
   }
 }
 
-class CardMenuRow1 extends StatelessWidget {
-  const CardMenuRow1(this.menuIndex, {Key? key}) : super(key: key);
+class CardMenuRow extends StatelessWidget {
+  const CardMenuRow(this.menuIndex, {Key? key}) : super(key: key);
 
   final int menuIndex;
 
@@ -117,62 +103,7 @@ class CardMenuRow1 extends StatelessWidget {
                 children: [
                   Icon(Icons.receipt_long_sharp),
                   SizedBox(height: 10,),
-                  Text('전자영수증'),
-                ],
-              ),
-
-            // 나만의 메뉴
-            if( menuIndex == 1)
-              Column(
-                children: [
-                  Icon(Icons.coffee),
-                  SizedBox(height: 10,),
-                  Text('나만의 메뉴'),
-                ],
-              ),
-
-            // 장바구니
-            if( menuIndex == 2)
-              Column(
-                children: [
-                  Icon(Icons.shopping_bag_outlined),
-                  SizedBox(height: 10,),
-                  Text('장바구니'),
-                ],
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CardMenuRow2 extends StatelessWidget {
-  const CardMenuRow2(this.menuIndex, {Key? key}) : super(key: key);
-
-  final int menuIndex;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(12))
-      ),
-
-      child: SizedBox(
-        width: sizedBoxWidth,
-        height: sizedBoxHeight,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            // 내 카드 관리
-            if( menuIndex == 0 )
-              Column(
-                children: [
-                  Icon(CupertinoIcons.creditcard),
-                  SizedBox(height: 10,),
-                  Text('내 카드 관리'),
+                  Text(cardMenuRow[0]),
                 ],
               ),
 
@@ -182,7 +113,7 @@ class CardMenuRow2 extends StatelessWidget {
                 children: [
                   Icon(Icons.person),
                   SizedBox(height: 10,),
-                  Text('개인정보 관리'),
+                  Text(cardMenuRow[1]),
                 ],
               ),
 
@@ -192,7 +123,7 @@ class CardMenuRow2 extends StatelessWidget {
                 children: [
                   Icon(Icons.settings),
                   SizedBox(height: 10,),
-                  Text('설정'),
+                  Text(cardMenuRow[2]),
                 ],
               ),
           ],
