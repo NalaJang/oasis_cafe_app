fba27c8c154f1a80a9c8f58dbdea0e1070e9650f
nala0897@naver.com <nala0897@naver.com>
2023-09-24 20:07:44 +1300

    Add:stateful widget 으로 변경하고 얼음 선택 옵션 UI 추가

--- lib/screens/personalOption/selectedFreshJuiceItem.dart
+++ lib/screens/personalOption/selectedFreshJuiceItem.dart
@@ -3,32 +3,89 @@ import 'package:oasis_cafe_app/config/palette.dart';
 
 import '../../strings/strings.dart';
 
-class SelectedFreshJuiceItem extends StatelessWidget {
+class SelectedFreshJuiceItem extends StatefulWidget {
   const SelectedFreshJuiceItem({required this.documentSnapshot, Key? key}) : super(key: key);
 
   final documentSnapshot;
 
   @override
+  State<SelectedFreshJuiceItem> createState() => _SelectedFreshJuiceItemState();
+}
+
+class _SelectedFreshJuiceItemState extends State<SelectedFreshJuiceItem> {
+
+  var iceOption = ['Less', 'Regular'];
+  var selectedIcoOption = [];
+
+  Color setExpansionPanelBackgroundColor() {
+    return const Color.fromARGB(250, 250, 250, 250);
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Column(
       children: [
-        // 커피
-        ListTile(
-          title: const Text(
-            '얼음',
-            style: TextStyle(
-                fontSize: 17
-            ),
-          ),
+        ExpansionPanelList.radio(
+          elevation: 0,
+          children: [
+            ExpansionPanelRadio(
+              value: 0,
+              // header 를 클릭했을 때도 펼치고 접을 수 있도록 설정
+              canTapOnHeader: true,
+              backgroundColor: setExpansionPanelBackgroundColor(),
+              headerBuilder: (BuildContext context, bool isExpanded) {
+                return ListTile(
+                  title: const Text(
+                    '얼음',
+                    style: TextStyle(
+                      fontSize: 17
+                    ),
+                  ),
 
-          subtitle: Text(
-              '${documentSnapshot['ice']}'
-          ),
+                  subtitle: Text(
+                    selectedIcoOption.isEmpty ? iceOption[1] : selectedIcoOption[0]
+                  )
+                );
+              },
 
-          trailing: Icon(Icons.arrow_forward_ios),
-        ),
+              body: Row(
+                children: [
+                  for( var i = 0; i < iceOption.length; i++ )
+                  GestureDetector(
 
-        Divider(height: 5, thickness: 1,),
+                    onTap: (){
+                      selectedIcoOption.clear();
+                      selectedIcoOption.add(iceOption[i]);
+                    },
+
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                        color: selectedIcoOption.contains(iceOption[i]) ?
+                                Palette.buttonColor1 : Colors.white
+                      ),
+
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(
+                          iceOption[i],
+                          style: TextStyle(
+                            color: selectedIcoOption.contains(iceOption[i]) ?
+                                    Colors.white : Colors.grey
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            )
+          ],
+        )
+        // Divider(height: 5, thickness: 1,),
       ],
     );
   }
