b7864788d1b6627c874764c1b8d5614794827828
nala0897@naver.com <nala0897@naver.com>
2023-09-25 17:43:57 +1300

    Add:서버에서 카트 데이터 가져오기

--- lib/model/model_cartItem.dart
+++ lib/model/model_cartItem.dart
@@ -3,12 +3,26 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class CartItemModel {
 
   late String itemName;
+  late String itemPrice;
   late String drinkSize;
   late String cup;
+  late String espressoOption;
+  late String hotOrIced;
+  late String syrupOption;
+  late String whippedCreamOption;
+  late String iceOption;
 
   CartItemModel.getSnapshotDataFromCart(DocumentSnapshot snapshot) {
     Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
 
+    itemName = data['itemName'];
+    itemPrice = data['itemPrice'];
     drinkSize = data['drinkSize'];
+    cup = data['cup'];
+    espressoOption = data['espressoOption'];
+    hotOrIced = data['hotOrIced'];
+    syrupOption = data['syrupOption'];
+    whippedCreamOption = data['whippedCreamOption'];
+    iceOption = data['iceOption'];
   }
 }
