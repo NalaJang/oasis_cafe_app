d400e03435cab13fe2e1ad117473cddc2b1196e4
nala0897@naver.com <nala0897@naver.com>
2023-09-26 15:45:51 +1300

    Fix:카트 아이템 list 코드를 클래스로 분리

--- lib/screens/mainMenus/order/cart.dart
+++ lib/screens/mainMenus/order/cart.dart
@@ -10,77 +10,101 @@ class Cart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final userStateProvider = Provider.of<UserStateProvider>(context);
-    final itemProvider = Provider.of<ItemProvider>(context);
-    final String userUid = userStateProvider.userUid;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('CartPage'),
       ),
 
-      body: FutureBuilder(
-        future: itemProvider.getItemsFromCart(userUid),
-        builder: (context, snapshot) {
-          return ListView.builder(
-            itemCount: itemProvider.cartItems.length,
-            itemBuilder: (context, index) {
-              String itemName = itemProvider.cartItems[index].itemName;
-              String itemPrice = itemProvider.cartItems[index].itemPrice;
-              String drinkSize = itemProvider.cartItems[index].drinkSize;
-              String cup = itemProvider.cartItems[index].cup;
-              int espressoOption = itemProvider.cartItems[index].espressoOption;
-              String hotOrIced = itemProvider.cartItems[index].hotOrIced;
-              String syrupOption = itemProvider.cartItems[index].syrupOption;
-              String whippedCreamOption = itemProvider.cartItems[index].whippedCreamOption;
-              String iceOption = itemProvider.cartItems[index].iceOption;
+      body: CartItems(),
 
-              return Row(
-                children: [
-                  Image.asset(
-                    'image/IMG_espresso.png',
-                    scale: 3.0,
-                  ),
-
-                  // 아이템 이름
-                  Column(
-                    children: [
-                      Text(
-                        '${itemName}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-
-                      Text(drinkSize),
-                      Text(cup),
-                      // Text(hotOrIced),
-                      // 수량 및 가격
-                      Row(
-                        children: [
-                          GestureDetector(
-                            onTap: (){},
-                            child: Icon(CupertinoIcons.minus_circle),
-                          ),
-
-                          Text('1'),
-
-                          GestureDetector(
-                            onTap: (){},
-                            child: Icon(CupertinoIcons.plus_circle),
-                          ),
-
-                          Text('${itemPrice}'),
-                        ],
-                      )
-                    ],
-                  ),
-                ],
-              );
-            }
-          );
-        }
-      ),
     );
   }
 }
+
+class CartItems extends StatelessWidget {
+  const CartItems({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final userStateProvider = Provider.of<UserStateProvider>(context);
+    final itemProvider = Provider.of<ItemProvider>(context);
+    final String userUid = userStateProvider.userUid;
+
+    return FutureBuilder(
+        future: itemProvider.getItemsFromCart(userUid),
+        builder: (context, snapshot) {
+          return ListView.builder(
+              itemCount: itemProvider.cartItems.length,
+              itemBuilder: (context, index) {
+                String itemName = itemProvider.cartItems[index].itemName;
+                String itemPrice = itemProvider.cartItems[index].itemPrice;
+                String drinkSize = itemProvider.cartItems[index].drinkSize;
+                String cup = itemProvider.cartItems[index].cup;
+                int espressoOption = itemProvider.cartItems[index].espressoOption;
+                String hotOrIced = itemProvider.cartItems[index].hotOrIced;
+                String syrupOption = itemProvider.cartItems[index].syrupOption;
+                String whippedCreamOption = itemProvider.cartItems[index].whippedCreamOption;
+                String iceOption = itemProvider.cartItems[index].iceOption;
+
+                return Row(
+                  children: [
+                    Image.asset(
+                      'image/IMG_espresso.png',
+                      scale: 3.0,
+                    ),
+
+                    // 아이템 이름
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${itemName}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+
+                        // hotOrIced, 사이즈, 컵 옵션
+                        Row(
+                          children: [
+                            Text(hotOrIced),
+                            Text(' | '),
+                            Text(drinkSize),
+                            Text(' | '),
+                            Text(cup)
+                          ],
+                        ),
+
+                        // 옵션 사항
+
+
+                        // 수량 및 가격
+                        Row(
+                          children: [
+                            GestureDetector(
+                              onTap: (){},
+                              child: Icon(CupertinoIcons.minus_circle),
+                            ),
+
+                            Text('1'),
+
+                            GestureDetector(
+                              onTap: (){},
+                              child: Icon(CupertinoIcons.plus_circle),
+                            ),
+
+                            Text('${itemPrice}'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                );
+              }
+          );
+        }
+    );
+  }
+}
+
