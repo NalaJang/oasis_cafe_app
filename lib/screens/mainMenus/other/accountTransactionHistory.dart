3d8e9a57cb57c922a3f4d96ac24f13373a34ecd1
nala0897@naver.com <nala0897@naver.com>
2023-10-06 14:43:34 +1300

    Fix:Column 만으로도 내가 원하는 고정된 상단바 구현이 가능했다..

--- lib/screens/mainMenus/other/accountTransactionHistory.dart
+++ lib/screens/mainMenus/other/accountTransactionHistory.dart
@@ -50,76 +50,56 @@ class AccountTransactionHistory extends StatelessWidget {
         title: Text('Transaction History'),
       ),
 
-      body: CustomScrollView(
-        // 스크롤 막기
-        // physics: NeverScrollableScrollPhysics(),
-        slivers: [
-          SliverAppBar(
-            title: Column(
-              children: [
-                Text('총 22건'),
-                Text('2022.10.07 - 2023.10.06'),
-                ElevatedButton(onPressed: (){print('clicked button');}, child: Text('button')
-                )
-              ],
-            ),
-            scrolledUnderElevation: 0,
-            // 뒤로 가기 화살표 제거
-            automaticallyImplyLeading: false,
-            expandedHeight: 100,
-            // 리스트를 내려도 헤더 고정
-            pinned: true,
-          ),
+      body: Column(
+        children: [
+          Text('ddddd'),
+          Divider(height: 10, thickness: 1, color: Colors.grey,),
+          FutureBuilder(
+            future: transactionHistoryProvider.getOrderHistory(userUid, year.toString(), month.toString(), '5'),
+            builder: (context, snapshot) {
 
-          SliverToBoxAdapter(
-            child: FutureBuilder(
-              future: transactionHistoryProvider.getOrderHistory(userUid, year.toString(), month.toString(), day.toString()),
-              builder: (context, snapshot) {
+              if( transactionHistoryProvider.historyList.isEmpty ) {
+                return Text('is empty');
 
-                if( transactionHistoryProvider.historyList.isEmpty ) {
-                  return Text('is empty');
+              } else {
+                return ListView.separated(
+                  shrinkWrap: true,
+                  separatorBuilder: (BuildContext context, int index) => Divider(
+                    color: Colors.grey,
+                  ),
 
-                } else {
-                  return ListView.separated(
-                    shrinkWrap: true,
-                    separatorBuilder: (BuildContext context, int index) => Divider(
-                      color: Colors.grey,
-                    ),
+                  itemCount: transactionHistoryProvider.historyList.length,
+                  itemBuilder: (context, index) {
+                    String price = transactionHistoryProvider.historyList[index].itemPrice;
+                    String itemName = transactionHistoryProvider.historyList[index].itemName;
+                    String time = transactionHistoryProvider.historyList[index].id;
 
-                    itemCount: transactionHistoryProvider.historyList.length,
-                    itemBuilder: (context, index) {
-                      String price = transactionHistoryProvider.historyList[index].itemPrice;
-                      String itemName = transactionHistoryProvider.historyList[index].itemName;
-                      String time = transactionHistoryProvider.historyList[index].id;
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 0),
+                      child: ListTile(
 
-                      return Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 0),
-                        child: ListTile(
+                        // 결제 수단
+                        leading: Icon(Icons.credit_card),
 
-                          // 결제 수단
-                          leading: Icon(Icons.credit_card),
+                        // 가격
+                        title: Text('NZD $price'),
 
-                          // 가격
-                          title: Text('NZD $price'),
-
-                          // 결제한 아이템 이름
-                          subtitle: Container(
-                              margin: EdgeInsets.only(top: 10),
-                              child: Text(itemName)
-                          ),
-
-                          // 결제 시간
-                          trailing: Text(time),
+                        // 결제한 아이템 이름
+                        subtitle: Container(
+                            margin: EdgeInsets.only(top: 10),
+                            child: Text(itemName)
                         ),
-                      );
-                    },
-                  );
-                }
-              },
-            ),
-          )
-        ],
 
+                        // 결제 시간
+                        trailing: Text(time),
+                      ),
+                    );
+                  },
+                );
+              }
+            },
+          ),
+        ],
       ),
     );
   }
