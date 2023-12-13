80f6933c16bb8328effbcd7ea4d74819fda5fb06
nala0897@naver.com <nala0897@naver.com>
2023-12-13 09:27:26 +0900

    Fix:코드에 const 추가

--- lib/screens/mainMenus/order/cart.dart
+++ lib/screens/mainMenus/order/cart.dart
@@ -18,13 +18,13 @@ class Cart extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Review Order'),
+        title: const Text('Review Order'),
       ),
 
       // 주문하기
-      bottomNavigationBar: _OrderButton(),
+      bottomNavigationBar: const _OrderButton(),
 
-      body: CartItems(),
+      body: const CartItems(),
 
     );
   }
@@ -51,7 +51,7 @@ class _CartItemsState extends State<CartItems> {
           context: context,
           builder: (BuildContext context) {
             return AlertDialog(
-              content: Text('선택한 메뉴를 삭제하시겠습니까?',),
+              content: const Text('선택한 메뉴를 삭제하시겠습니까?',),
               actions: [
 
                 // 취소 버튼
@@ -60,21 +60,21 @@ class _CartItemsState extends State<CartItems> {
                     Navigator.of(context).pop();
                   },
 
-                  child: Text('아니오'),
-
                   style: ElevatedButton.styleFrom(
                     elevation: 0,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(18)
                     ),
-                    side: BorderSide(
+                    side: const BorderSide(
                       color: Colors.teal,
                       width: 1
                     )
                   ),
+
+                  child: const Text('아니오'),
                 ),
 
-                SizedBox(width: 10,),
+                const SizedBox(width: 10,),
 
                 // 삭제 버튼
                 ElevatedButton(
@@ -111,8 +111,6 @@ class _CartItemsState extends State<CartItems> {
 
                   },
 
-                  child: Text('삭제'),
-
                   style: ElevatedButton.styleFrom(
                     elevation: 0,
                     foregroundColor: Colors.white,
@@ -120,11 +118,13 @@ class _CartItemsState extends State<CartItems> {
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(18)
                     ),
-                    side: BorderSide(
+                    side: const BorderSide(
                         color: Colors.teal,
                         width: 1
                     )
                   ),
+
+                  child: const Text('삭제'),
                 )
               ],
             );
@@ -188,7 +188,7 @@ class _CartItemsState extends State<CartItems> {
                             onPressed: (){
                               setShowDialog(itemId);
                             },
-                            icon: Icon(CupertinoIcons.xmark_circle)
+                            icon: const Icon(CupertinoIcons.xmark_circle)
                         ),
                       ],
                     ),
@@ -202,42 +202,42 @@ class _CartItemsState extends State<CartItems> {
 
                         // 아이템 정보
                         Container(
-                          margin: EdgeInsets.only(left: 20),
+                          margin: const EdgeInsets.only(left: 20),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                '${itemName}',
-                                style: TextStyle(
+                                itemName,
+                                style: const TextStyle(
                                     fontSize: 17,
                                     fontWeight: FontWeight.bold
                                 ),
                               ),
 
-                              SizedBox(height: 10,),
+                              const SizedBox(height: 10,),
 
                               // hotOrIced, 사이즈, 컵 옵션
                               Row(
                                 children: [
                                   Text(hotOrIced),
-                                  Text(' | '),
+                                  const Text(' | '),
                                   Text(drinkSize),
-                                  Text(' | '),
+                                  const Text(' | '),
                                   Text(cup)
                                 ],
                               ),
 
                               //// 옵션 사항
                               // 에스프레소
-                              espressoOption != 2 ? Text('$espressoOption') : SizedBox(height: 0,),
+                              espressoOption != 2 ? Text('$espressoOption') : const SizedBox(height: 0,),
                               // 시럽
-                              syrupOption != "" ? Text(syrupOption) : SizedBox(height: 0,),
+                              syrupOption != "" ? Text(syrupOption) : const SizedBox(height: 0,),
                               // 휘핑 크림
-                              whippedCreamOption != "" ? Text(whippedCreamOption) : SizedBox(height: 0,),
+                              whippedCreamOption != "" ? Text(whippedCreamOption) : const SizedBox(height: 0,),
                               // 얼음
-                              iceOption != "" ? Text('얼음 $iceOption') : SizedBox(height: 0,),
+                              iceOption != "" ? Text('얼음 $iceOption') : const SizedBox(height: 0,),
 
-                              SizedBox(height: 20,),
+                              const SizedBox(height: 20,),
 
                               // 수량 및 가격
                               Row(
@@ -258,8 +258,8 @@ class _CartItemsState extends State<CartItems> {
                                       ),
 
                                       Container(
-                                        margin: EdgeInsets.only(left: 20, right: 20),
-                                        child: Text('${quantity}'),
+                                        margin: const EdgeInsets.only(left: 20, right: 20),
+                                        child: Text('$quantity'),
                                       ),
 
                                       GestureDetector(
@@ -267,13 +267,13 @@ class _CartItemsState extends State<CartItems> {
                                           quantity++;
                                           setQuantity(itemId, quantity, double.parse(itemPrice));
                                         },
-                                        child: Icon(CupertinoIcons.plus_circle),
+                                        child: const Icon(CupertinoIcons.plus_circle),
                                       ),
 
                                     ],
                                   ),
 
-                                  SizedBox(width: 50,),
+                                  const SizedBox(width: 50,),
 
                                   Text('NZD $totalPrice')
                                 ],
@@ -289,7 +289,7 @@ class _CartItemsState extends State<CartItems> {
             },
           );
         }
-        return Center(child: CircularProgressIndicator());
+        return const Center(child: CircularProgressIndicator());
       }
     );
   }
@@ -303,7 +303,7 @@ class _OrderButton extends StatefulWidget {
 }
 
 class _OrderButtonState extends State<_OrderButton> {
-  var color = Color(0xffe8e8e8);
+  var color = const Color(0xffe8e8e8);
 
   @override
   Widget build(BuildContext context) {
@@ -323,7 +323,7 @@ class _OrderButtonState extends State<_OrderButton> {
             color: color,
             spreadRadius: 5,
             blurRadius: 10,
-            offset: Offset(3, 0)
+            offset: const Offset(3, 0)
           )
         ]
 
@@ -392,8 +392,8 @@ class _OrderButtonState extends State<_OrderButton> {
         },
 
         child: Container(
-          padding: EdgeInsets.all(15.0),
-          margin: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 30),
+          padding: const EdgeInsets.all(15.0),
+          margin: const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 30),
 
           decoration: BoxDecoration(
             color: Palette.buttonColor1,
