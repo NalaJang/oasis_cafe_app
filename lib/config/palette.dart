23dd45a0113e71cbccb607650be886cfb3aaa7c7
nala0897@naver.com <nala0897@naver.com>
2023-12-14 14:50:08 +0900

    Fix:button1 색상 수정

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -4,7 +4,7 @@ class Palette {
   static const Color iconColor = Color(0xFFB6C7D1);
   static const Color textColor1 = Color(0xFF232323);
   static const Color textColor2 = Color(0XFFA7BCC7);
-  static const Color buttonColor1 = Color(0xFF1AB5DB);
+  static const Color buttonColor1 = Color(0xFF12744B);
   static const Color buttonColor2 = Color(0xFFCDCBCB);
   static const Color backgroundColor = Color(0xFFFFFFFF);
 }
