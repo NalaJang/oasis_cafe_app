0965f14ef8bd75c4c60211e4f9a4213a0dc816e1
nala0897@naver.com <nala0897@naver.com>
2024-02-09 18:10:30 +0900

    주문 카드의 조건문 코드 정리

--- lib/screens/mainMenus/main/orderStatus/orderStatusWidget.dart
+++ lib/screens/mainMenus/main/orderStatus/orderStatusWidget.dart
@@ -1,10 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../config/circularProgressBar.dart';
-import '../../../../config/palette.dart';
-import '../../../../localNotification.dart';
 import '../../../../provider/orderStateProvider.dart';
 import '../../../../provider/userStateProvider.dart';
 import 'orderProcessStateCard.dart';
@@ -18,6 +15,7 @@ class OrderStatus extends StatefulWidget {
 
 class _OrderStatusState extends State<OrderStatus> {
 
+  late OrderStatusInfo orderStatusInfo;
 
   @override
   void initState() {
@@ -36,6 +34,7 @@ class _OrderStatusState extends State<OrderStatus> {
   Widget build(BuildContext context) {
 
     var userStateProvider = Provider.of<UserStateProvider>(context);
+    String userName = userStateProvider.userName;
 
     // 로그인 상태인지 확인
     if( userStateProvider.userUid == '' ) {
@@ -43,8 +42,12 @@ class _OrderStatusState extends State<OrderStatus> {
 
     } else {
 
+      if( userName == '' ) {
+        userStateProvider.getStorageInfo();
+        return CircularProgressBar.circularProgressBar;
+      }
+
       var orderStateProvider = Provider.of<OrderStateProvider>(context);
-      String userName = userStateProvider.userName;
 
       /*
     .where('processState', isNotEqualTo: 'done') ==> isEqualTo -> isNotEqualTo 로 변경하자 발생한 에러.
@@ -57,70 +60,43 @@ class _OrderStatusState extends State<OrderStatus> {
     .orderBy('processState') 를 추가해주었다.
     */
       return StreamBuilder(
-          stream: orderStateProvider.orderStateCollection
-              .where('processState', isNotEqualTo: 'pickedUp')
-              .orderBy('processState')
-              .orderBy('orderTime', descending: false)
-              .snapshots(),
+        stream: orderStateProvider.orderStateCollection
+                                  .where('processState', isNotEqualTo: 'pickedUp')
+                                  .orderBy('processState')
+                                  .orderBy('orderTime', descending: false)
+                                  .snapshots(),
 
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              if (snapshot.data!.size == 0) {
-                return noOrder();
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data!.size == 0) {
+              return noOrder();
 
-              } else {
-                bool orderCanceled = false;
-                var document = snapshot.data!.docs[0]; // [0] 제일 먼저 주문한 메뉴를 가지고 온다.
-                var documentId = document.id;
-                var processState = document['processState'];
-                var splitProcessState = processState.toString().split(':');
-                if( splitProcessState[0] != null ) {
-                  processState = splitProcessState[0];
-                }
-                String cardTitlePhrase = '';
-                String cardSubTitlePhrase = '';
-                String graphImage = '';
-
-                if (processState == 'new') {
-                  cardTitlePhrase = '주문을 확인하고 있습니다. 🏃🏻‍♀️';
-                  cardSubTitlePhrase =
-                  '주문 상황에 따라 준비가 늦어질 수 있습니다. 본인이 직접 메뉴를 수령해 주세요.';
-                  graphImage = 'image/IMG_order_status_new.png';
-
-                } else if (processState == 'inProcess') {
-                  cardTitlePhrase = '$userName 님의 주문을 준비 중입니다.';
-                  cardSubTitlePhrase = '주문 승인 즉시 메뉴 준비가 시작됩니다. 완성 후, 빠르게 픽업해 주세요.';
-                  graphImage = 'image/IMG_order_status_inProcess.png';
-
-                } else if (processState == 'done') {
-                  cardTitlePhrase = '$userName 님, 메뉴가 모두 준비되었어요.';
-                  cardSubTitlePhrase = '메뉴가 모두 준비되었어요. 픽업대에서 메뉴를 픽업해주세요!';
-                  graphImage = 'image/IMG_order_status_done.png';
-
-                } else if( processState == 'canceled' ) {
-                  orderCanceled = true;
-                  var reasonOfCancel = splitProcessState[1];
-                  cardTitlePhrase = '$userName 님, 주문이 취소되었어요.';
-                  cardSubTitlePhrase = '$reasonOfCancel (으)로 주문이 취소되었습니다.';
-                  graphImage = '';
-                }
-
-
-                // 푸시 알림으로 메뉴 준비 상태 알리기
-                // LocalNotification().showNotification(processState);
-
-                // 카드 이미지
-                return OrderProcessStateCard(
-                  orderCanceled: orderCanceled,
-                  cardTitlePhrase: cardTitlePhrase,
-                  cardSubTitlePhrase: cardSubTitlePhrase,
-                  graphImage: graphImage,
-                  documentSnapshot: document
-                );
+            } else {
+              var document = snapshot.data!.docs[0]; // [0] 제일 먼저 주문한 메뉴를 가지고 온다.
+              var processState = document['processState'];
+              var splitProcessState = processState.toString().split(':');
+              if( splitProcessState[0] != '' ) {
+                processState = splitProcessState[0];
               }
+
+              // 주문 상태에 따른 안내 정보
+              orderStatusInfo = getOrderStatus(processState, userName, splitProcessState);
+
+              // 푸시 알림으로 메뉴 준비 상태 알리기
+              // LocalNotification().showNotification(processState);
+
+              // 카드 이미지
+              return OrderProcessStateCard(
+                orderCanceled: orderStatusInfo.isCanceled,
+                cardTitlePhrase: orderStatusInfo.title,
+                cardSubTitlePhrase: orderStatusInfo.subTitle,
+                graphImage: orderStatusInfo.graphImage,
+                documentSnapshot: document
+              );
             }
-            return CircularProgressBar.circularProgressBar;
           }
+          return CircularProgressBar.circularProgressBar;
+        }
       );
     }
   }
@@ -134,4 +110,56 @@ class _OrderStatusState extends State<OrderStatus> {
       ),
     );
   }
+
+  // 주문 상태에 따른 안내 정보
+  OrderStatusInfo getOrderStatus(String processState, String userName, List<String> splitProcessState) {
+    bool orderCanceled = false;
+    String title = '';
+    String subTitle = '';
+    String graphImage = '';
+
+    if (processState == 'new') {
+      title = '주문을 확인하고 있습니다. 🏃🏻‍♀️';
+      subTitle = '주문 상황에 따라 준비가 늦어질 수 있습니다. 본인이 직접 메뉴를 수령해 주세요.';
+      graphImage = 'image/IMG_order_status_new.png';
+
+    } else if (processState == 'inProcess') {
+      title = '$userName 님의 주문을 준비 중입니다.';
+      subTitle = '주문 승인 즉시 메뉴 준비가 시작됩니다. 완성 후, 빠르게 픽업해 주세요.';
+      graphImage = 'image/IMG_order_status_inProcess.png';
+
+    } else if (processState == 'done') {
+      title = '$userName 님, 메뉴가 모두 준비되었어요.';
+      subTitle = '메뉴가 모두 준비되었어요. 픽업대에서 메뉴를 픽업해주세요!';
+      graphImage = 'image/IMG_order_status_done.png';
+
+    } else if( processState == 'canceled' ) {
+      orderCanceled = true;
+      var reasonOfCancel = splitProcessState[1];
+      title = '$userName 님, 주문이 취소되었어요.';
+      subTitle = '$reasonOfCancel (으)로 주문이 취소되었습니다.';
+      graphImage = '';
+    }
+
+    return OrderStatusInfo(
+      title: title,
+      subTitle: subTitle,
+      graphImage: graphImage,
+      isCanceled: orderCanceled
+    );
+  }
+}
+
+class OrderStatusInfo {
+  final String title;
+  final String subTitle;
+  final String graphImage;
+  final bool isCanceled;
+
+  OrderStatusInfo({
+    required this.title,
+    required this.subTitle,
+    required this.graphImage,
+    this.isCanceled = false
+  });
 }
